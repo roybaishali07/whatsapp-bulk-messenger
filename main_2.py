5684c9bb991ba214618b2b3339179809ce6e426e
Anirban Dutta <61903332+DuttaAB-dev@users.noreply.github.com>
2023-04-02 17:24:19 +0000

    Imported Required Packages

--- main_2.py
+++ main_2.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 # Config
